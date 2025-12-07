44dbdde8cb999e0a22f296d9f18ca80db3c67790
WahyuSattriana <wahyusattriana@gmail.com>
2025-12-08 02:16:38 +0800

    feat: add analytics and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/next';
+import Footer from '@/components/footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +29,9 @@ export default function RootLayout({
     <html lang="en" className="light" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Footer />
         <Toaster position="top-center" theme="light" />
+        <Analytics />
       </body>
     </html>
   );
