f73bbe0df931a3523fce64aaa521a6e76d16f7af
WahyuSattriana <wahyusattriana@gmail.com>
2025-12-07 23:44:30 +0800

    feat: add GET /api/user

---
+++ app/api/user/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get('username');
+
+    if (!username) {
+      return NextResponse.json({ message: 'Username is required' }, { status: 400 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+      select: {
+        username: true,
+        fullname: true,
+        status: true,
+        choice: true
+      }
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: 'User fetched',
+        user
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
